12b336e2f38f421c5b62b54612eb5497435abee9
AlanWeik <alaan.m@hotmail.com>
2021-11-11 13:04:19 +0100

    First commit. Navbar wont extend, white div wont go away.

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+  <p>
+    This is your footer
+  </p>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
